5a468c80d3ec62f6a77f737d1762a3ff4105a852
Jonathan Brincat <jonathan@pix8.co.uk>
2024-02-23 02:27:59 +0000

    resolved the problem of caching a reference to previous state made difficult by Reacts reactivity model

--- examples/react-app/src/react/components/Digit.tsx
+++ examples/react-app/src/react/components/Digit.tsx
@@ -1,29 +1,56 @@
+import React, { useState, useRef, useEffect } from 'react'
+
 import './Digit.css'
 
-const state = {
-  toggle: false,
-  previousValue: 0,
-}
+export default function Digit({ value }: { value: number}) {
+  const [toggle, setToggle] = useState(false)
+  const [previousValue, setPreviousValue] = useState(value)
+  const foo: React.MutableRefObject<T | null> = useRef(null)
+  const bar: React.MutableRefObject<T | null> = useRef(null)
 
-export default function Digit({ value }) {
+  useEffect(() => {
+    // console.log('top :: ', foo.current)
+    // console.log('bottom :: ', bar.current)
+
+    if (foo.current) {
+      foo.current?.addEventListener('transitionstart', (event: React.SyntheticEvent<T>) => {
+        console.log('transitionstart :: top')
+      })
+      foo.current?.addEventListener('transitionend', (event: React.SyntheticEvent<T>) => {
+        console.log('transitionend :: bottom')
+      })
+    }
+
+    return () => {
+      if (foo.current) {
+        foo.current?.removeEventListener('transitionstart', (event: React.SyntheticEvent<T>) => { })
+        foo.current?.removeEventListener('transitionend', (event: React.SyntheticEvent<T>) => { })
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    return () => setPreviousValue(value)
+  }, [value])
+
   return (
     <div className="ui__digit">
       <div>
           {/* <DEVNOTE: Modulus operand is enforced as a guard to safeguard single-digit usage */}
-          <div className="digit" data-before={Math.abs(value % 10)} data-after={Math.abs(state.previousValue % 10)}>
+          <div className="digit" data-before={Math.abs(value % 10)} data-after={Math.abs(previousValue % 10)}>
           {/* <Transition name="card-top"> */}
             {/* DEVNOTE: alternative to using v-if with v-show; no pitfalls for doing it this way either */}
-            <div className="card card-top">{state.toggle ? Math.abs(value % 10) : Math.abs(state.previousValue % 10)}</div>
+          <div className="card card-top" ref={foo} v-show="toggle" style={{ display: toggle ? 'none' : 'flex' }}>{toggle ? Math.abs(value % 10) : Math.abs(previousValue % 10)}</div>
 
-            {/* DEVNOTE: here I exploit v-if and the mechanic of injecting into the dom before the reactivity can effect a repaint to cache the previos value and update with a fresh pain after the transition. This avoids having to otherwise manage and orchestrate with logic and removes other potential overheads such as rolling over zero between base-10 and base-6(which would otherwise need to be handled), and by limiting the control logic and the component functionally pure(you feed a prop in and the same prop output pops out) it can handle incrementing and decrementing values without intervention; */}
-            {/* although a v-if is more expensive than a v-show. I feel the advantages + development experience outweigh the cost + there is a reduced logic footprint so less to execute so you're getting some return */}
-            <div className="card card-top" v-if="state.toggle">{Math.abs(value % 10)}</div>
+            {/* <div className="card card-top" v-if="toggle">{Math.abs(value % 10)}</div> */}
           {/* </Transition> */}
 
           {/* <Transition name="card-bottom" @after-leave="onDigitAfterLeave"> */}
-            <div className="card card-bottom" v-show="state.toggle">{Math.abs(value % 10)}</div>
+            <div className="card card-bottom" ref={bar} v-show="toggle" style={{ display: toggle ? 'none' : 'flex' }}>{Math.abs(value % 10)}</div>
           {/* </Transition> */}
         </div>
+        <p className="text-xs">toggle: {JSON.stringify(toggle)}</p>
+        <p className="text-xs">previousValue: {JSON.stringify(previousValue)}</p>
       </div>
     </div>
   )
